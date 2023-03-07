343e73bd61cc895ac43a30a21cf107e961fa1e4f
ondastudio <75018767+ondastudio@users.noreply.github.com>
2023-03-07 15:56:49 +0000

    Create swiper.js

---
+++ swiper.js
@@ -0,0 +1,175 @@
+const swiper = new Swiper("#swiper1", {
+  followFinger: true,
+  freeMode: true,
+  slideToClickedSlide: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  rewind: false,
+  breakpoints: {
+    // mobile landscape
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // tablet
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // desktop
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    }
+  },
+  scrollbar: {
+    el: ".swiper-drag-wrapper",
+    draggable: true,
+    dragClass: "swiper-drag",
+    snapOnRelease: true
+  },
+  slideActiveClass: "is-active",
+  slideDuplicateActiveClass: "is-active"
+});
+
+const swiper2 = new Swiper("#swiper2", {
+  followFinger: true,
+  freeMode: true,
+  slideToClickedSlide: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  rewind: false,
+  breakpoints: {
+    // mobile landscape
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // tablet
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // desktop
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    }
+  },
+  scrollbar: {
+    el: ".swiper-drag-wrapper-2",
+    draggable: true,
+    dragClass: "swiper-drag-2",
+    snapOnRelease: true
+  },
+  slideActiveClass: "is-active",
+  slideDuplicateActiveClass: "is-active"
+});
+
+const swiper3 = new Swiper("#swiper3", {
+  followFinger: true,
+  freeMode: true,
+  slideToClickedSlide: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  rewind: false,
+  breakpoints: {
+    // mobile landscape
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // tablet
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // desktop
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    }
+  },
+  scrollbar: {
+    el: ".swiper-drag-wrapper-3",
+    draggable: true,
+    dragClass: "swiper-drag-3",
+    snapOnRelease: true
+  },
+  slideActiveClass: "is-active",
+  slideDuplicateActiveClass: "is-active"
+});
+
+const swiper4 = new Swiper("#swiper4", {
+  followFinger: true,
+  slideToClickedSlide: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  autoplay: {
+    delay: 3000
+  },
+  rewind: false,
+  breakpoints: {
+    // mobile landscape
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // tablet
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // desktop
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    }
+  },
+  scrollbar: {
+    el: ".swiper-drag-wrapper-4",
+    draggable: true,
+    dragClass: "swiper-drag-4",
+    snapOnRelease: true
+  },
+  slideActiveClass: "is-active",
+  slideDuplicateActiveClass: "is-active"
+});
+
+const swiper5 = new Swiper("#swiper5", {
+  followFinger: true,
+  slideToClickedSlide: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  autoplay: {
+    delay: 3000
+  },
+  rewind: false,
+  breakpoints: {
+    // mobile landscape
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // tablet
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    },
+    // desktop
+    992: {
+      slidesPerView: "auto",
+      spaceBetween: 16
+    }
+  },
+  scrollbar: {
+    el: ".swiper-drag-wrapper-5",
+    draggable: true,
+    dragClass: "swiper-drag-5",
+    snapOnRelease: true
+  },
+  slideActiveClass: "is-active",
+  slideDuplicateActiveClass: "is-active"
+});
+
+//console.log(swiper4);
